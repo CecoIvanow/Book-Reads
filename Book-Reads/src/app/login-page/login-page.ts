886628d6ceea847b9df2f71f5daee4517fb5d1b0
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-06-28 23:29:23 +0300

    Chore: Test Angular Material works properly

--- Book-Reads/src/app/login-page/login-page.ts
+++ Book-Reads/src/app/login-page/login-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'login-page',
-  imports: [],
+  imports: [MatSlideToggleModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss'
 })
