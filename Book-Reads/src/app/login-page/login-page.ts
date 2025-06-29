5142d6d6bc265f9c7c1eec61177a81e97803752c
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-06-29 12:40:08 +0300

    Feat: Add login-page component

--- Book-Reads/src/app/login-page/login-page.ts
+++ Book-Reads/src/app/login-page/login-page.ts
@@ -2,21 +2,14 @@ import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSelectModule } from '@angular/material/select';
-
-interface Gender {
-    gender: 'Male' | 'Female';
-}
+import { MatRadioModule } from '@angular/material/radio';
 
 @Component({
     selector: 'login-page',
-    imports: [MatInputModule, MatFormFieldModule, MatSelectModule, MatButtonModule],
+    imports: [MatInputModule, MatFormFieldModule, MatRadioModule, MatButtonModule],
     templateUrl: './login-page.html',
     styleUrl: './login-page.scss',
 })
 export class LoginPage {
-    genders: Gender[] = [
-        { gender: 'Male' },
-        { gender: 'Female' }
-    ];
+
 }
