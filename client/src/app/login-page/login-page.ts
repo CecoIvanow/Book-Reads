dcf7bae5f04337730f0e413b876baab66178e9e9
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-06-29 21:43:09 +0300

    Feat: Add basic login request

--- client/src/app/login-page/login-page.ts
+++ client/src/app/login-page/login-page.ts
@@ -10,5 +10,19 @@ import { MatButtonModule } from '@angular/material/button';
     styleUrl: './login-page.scss',
 })
 export class LoginPage {
+    async onLogin(e: Event) {
+        e.preventDefault();
 
+        const formData = new FormData(e.currentTarget as HTMLFormElement);
+        const {password, email} = Object.fromEntries(formData);        
+
+        const resp = await fetch('http://localhost:3030/users/login', {
+            method: 'POST',
+            body: JSON.stringify({
+                password,
+                email
+            })
+        })
+        const data = await resp.json();
+    }
 }
