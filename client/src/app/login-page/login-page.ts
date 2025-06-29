77f5d90f0154b6f1baa051fab991da56b57a28b9
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-06-29 16:16:13 +0300

    Chore: Remove unused import from login-page component

--- client/src/app/login-page/login-page.ts
+++ client/src/app/login-page/login-page.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
-import { MatRadioModule } from '@angular/material/radio';
 
 @Component({
     selector: 'login-page',
-    imports: [MatInputModule, MatFormFieldModule, MatRadioModule, MatButtonModule],
+    imports: [MatInputModule, MatFormFieldModule, MatButtonModule],
     templateUrl: './login-page.html',
     styleUrl: './login-page.scss',
 })
