67afcb8a41afb3e86a8fbfb2fcfcbf58e13ba6a9
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-06-29 21:55:42 +0300

    Feat: Add basic register request

--- client/src/app/register-page/register-page.ts
+++ client/src/app/register-page/register-page.ts
@@ -7,15 +7,30 @@ import { MatRadioModule } from '@angular/material/radio';
 type Gender = 'Male' | 'Female';
 
 @Component({
-  selector: 'register-page',
-  imports: [MatInputModule, MatFormFieldModule, MatRadioModule, MatButtonModule],
-  templateUrl: './register-page.html',
-  styleUrl: './register-page.scss'
+    selector: 'register-page',
+    imports: [MatInputModule, MatFormFieldModule, MatRadioModule, MatButtonModule],
+    templateUrl: './register-page.html',
+    styleUrl: './register-page.scss'
 })
 
 export class RegisterPage {
-  genders: Gender[] = [
-    'Male',
-    'Female'
-  ];
+    genders: Gender[] = [
+        'Male',
+        'Female'
+    ]
+
+    async onRegister(e: Event) {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget as HTMLFormElement);
+        const registerData = Object.fromEntries(formData);
+
+        const resp = await fetch('http://localhost:3030/users/register', {
+            method: 'POST',
+            body: JSON.stringify({
+                ...registerData
+            })
+        })
+        const data = await resp.json();
+    }
 }
