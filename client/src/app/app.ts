b214f6ec773fbb3b960db8cdb12db09bfc469fb9
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-06-30 20:21:52 +0300

    Client/Feat: Add Book and UUIDv4 types

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { LoginPage } from './login-page/login-page.js';
 import { RegisterPage } from './register-page/register-page.js';
 import { NavBar } from './nav-bar/nav-bar.js';
+import { CatalogPage } from './catalog-page/catalog-page.js';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginPage, RegisterPage, NavBar],
+  imports: [RouterOutlet, LoginPage, RegisterPage, NavBar, CatalogPage],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
