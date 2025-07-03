e061acf858d0db38b7a7bbfe2f752579ff3905e0
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-07-03 23:13:09 +0300

    Client/Feat: Add LoginCredentials interface

--- client/src/app/core/auth/pages/login/login.ts
+++ client/src/app/core/auth/pages/login/login.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { LoginCredentials } from '../../models/login-credentials.model.js';
 
 @Component({
     selector: 'app-login',
@@ -14,14 +15,20 @@ export class Login {
         e.preventDefault();
 
         const formData = new FormData(e.currentTarget as HTMLFormElement);
-        const {password, email} = Object.fromEntries(formData);        
+        const email = formData.get('email') as string;        
+        const password = formData.get('password') as string;        
+
+        const credentials: LoginCredentials = {
+            email,
+            password,
+        }
 
         const resp = await fetch('http://localhost:3030/users/login', {
             method: 'POST',
-            body: JSON.stringify({
-                password,
-                email
-            })
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
         })
         const data = await resp.json();
     }
