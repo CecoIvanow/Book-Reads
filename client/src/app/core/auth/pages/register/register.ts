5b5e8f8e37b88303670f23373dfa6bdb7eb4252f
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-07-03 23:20:00 +0300

    Client/Feat: Add RegisterCredentials interface

--- client/src/app/core/auth/pages/register/register.ts
+++ client/src/app/core/auth/pages/register/register.ts
@@ -3,6 +3,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatRadioModule } from '@angular/material/radio';
+import { RegisterCredentials } from '../../models/register-credentials.model.js';
 
 type Gender = 'Male' | 'Female';
 
@@ -20,12 +21,21 @@ export class Register {
 
         const formData = new FormData(e.currentTarget as HTMLFormElement);
         const registerData = Object.fromEntries(formData);
+        const email = formData.get('email') as string;
+        const password = formData.get('password') as string;
+        const firstName = formData.get('firstName') as string;
+        const lastName = formData.get('lastName') as string;
+
+        const credentials: RegisterCredentials = {
+            email,
+            password,
+            firstName,
+            lastName
+        }
 
         const resp = await fetch('http://localhost:3030/users/register', {
             method: 'POST',
-            body: JSON.stringify({
-                ...registerData
-            })
+            body: JSON.stringify(credentials)
         })
         const data = await resp.json();
     }
