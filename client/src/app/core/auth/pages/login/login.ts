35d05423c4b5ad057a657d232b5d801d0ee5638d
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-07-04 00:41:36 +0300

    Client/Refactor: Implement login auth service call in login page

--- client/src/app/core/auth/pages/login/login.ts
+++ client/src/app/core/auth/pages/login/login.ts
@@ -3,6 +3,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { LoginCredentials } from '../../models/login-credentials.model.js';
+import { AuthService } from '../../auth.service.js';
+import { AuthResponse } from '../../models/auth-response.model.js';
 
 @Component({
     selector: 'app-login',
@@ -11,25 +13,24 @@ import { LoginCredentials } from '../../models/login-credentials.model.js';
     styleUrl: './login.scss',
 })
 export class Login {
-    async onLogin(e: Event) {
+    authResponse: AuthResponse | null = null; 
+
+    constructor(private authService: AuthService) {
+    }
+
+    onLogin(e: Event) {
         e.preventDefault();
 
         const formData = new FormData(e.currentTarget as HTMLFormElement);
-        const email = formData.get('email') as string;        
-        const password = formData.get('password') as string;        
+        const email = formData.get('email') as string;
+        const password = formData.get('password') as string;
 
         const credentials: LoginCredentials = {
             email,
             password,
         }
 
-        const resp = await fetch('http://localhost:3030/users/login', {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify(credentials)
-        })
-        const data = await resp.json();
+        this.authService.login(credentials).subscribe(data => this.authResponse = data);
+        
     }
 }
