3f73a70ffe7dd5886cf9f8567c2ad798d76357ec
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-07-05 01:35:55 +0300

    Client/WIP: Catalog page

--- client/src/app/features/books/pages/catalog/catalog.ts
+++ client/src/app/features/books/pages/catalog/catalog.ts
@@ -1,19 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { Book } from '../../book.model.js';
+import { BooksService } from '../../books.service.js';
 
 @Component({
     selector: 'app-catalog-page',
-    imports: [],
+    imports: [MatCardModule, MatButtonModule],
     templateUrl: './catalog.html',
     styleUrl: './catalog.scss'
 })
 export class Catalog {
     books$: Book[] | null = null;
 
-    constructor(private httpClient: HttpClient) {
+    constructor(private booksServices: BooksService) {
     }
 
-    async ngOnInit() {
+    ngOnInit() {
+        this.booksServices.getAllBooks().subscribe(data => this.books$ = data);
     }
 }
