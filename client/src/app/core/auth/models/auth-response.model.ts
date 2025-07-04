79e17b8c3fb50fd873529b20a6f5b52bc3cd960f
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-07-04 22:41:22 +0300

    Client/Refactor: Add UUISv4 type to _id property in AuthResponse type

--- client/src/app/core/auth/models/auth-response.model.ts
+++ client/src/app/core/auth/models/auth-response.model.ts
@@ -1,8 +1,10 @@
-export interface AuthResponse {
+import { UUIDv4 } from "../../../shared/models/index.js";
+
+export type AuthResponse = {
     accessToken: string,
     email: string,
     firstName: string,
     lastName: string,
     username: string,
-    _id: string,
+    _id: UUIDv4,
 }
