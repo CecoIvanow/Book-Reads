ce8c6d72fa83131b97d7695d924642f0a25cfe28
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-07-04 20:15:03 +0300

    Client/Refactor: Create a separate shared Book interface model in \app\features\books

---
+++ client/src/app/features/books/book.model.ts
@@ -0,0 +1,13 @@
+export interface Book {
+    _ownerId: UUIDv4,
+    title: string,
+    author: string,
+    img: string,
+    createdOn: string,
+    _id: UUIDv4,
+    likes: UUIDv4[],
+    comments: UUIDv4[],
+    summary: string
+}
+
+type UUIDv4 = string;
