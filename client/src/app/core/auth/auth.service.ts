196f8e5203d68ffaca4a2f1a3e00cb34c04e7c08
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-07-07 00:10:48 +0300

    Client/Refactor: add .util to api-url-builder

--- client/src/app/core/auth/auth.service.ts
+++ client/src/app/core/auth/auth.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { LoginCredentials, RegisterCredentials, AuthResponse } from './models/index.js';
 import { Observable } from 'rxjs';
 import { API_PATHS } from '../../shared/constants/index.js';
-import { buildURL } from '../../shared/utils/api-url-builder.js';
+import { buildURL } from '../../shared/utils/api-url-builder.util.js';
 
 @Injectable({
     providedIn: 'root'
@@ -15,7 +15,7 @@ export class AuthService {
     login(body: LoginCredentials): Observable<AuthResponse> {
         const url = buildURL(API_PATHS.USERS.LOGIN);
 
-        return this.httpClient.post<AuthResponse>(url , body)
+        return this.httpClient.post<AuthResponse>(url, body)
     }
 
     register(body: RegisterCredentials): Observable<AuthResponse> {
