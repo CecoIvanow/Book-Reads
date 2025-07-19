3f376e3879a960f099d9d47aa7960fc6ab606d31
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-07-19 22:56:35 +0300

    Client/Feat: add router link for /catalog

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,12 +2,10 @@ import { Routes } from '@angular/router';
 
 import { Login } from './core/auth/pages/login/login.js';
 import { Register } from './core/auth/pages/register/register.js';
+import { Catalog } from './features/books/pages/catalog/catalog.js';
 
 export const routes: Routes = [
-    {
-        path: 'login', component: Login,
-    },
-    {
-        path: 'register', component: Register,
-    }
+    { path: 'login', component: Login },
+    { path: 'register', component: Register },
+    { path: 'catalog', component: Catalog},
 ];
