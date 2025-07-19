3887b2c330b90eee2ff6a59eb9191f11d08fd7b5
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-07-19 22:43:20 +0300

    Client/Feat: add /login and /register routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Login } from './core/auth/pages/login/login.js';
+import { Register } from './core/auth/pages/register/register.js';
+
+export const routes: Routes = [
+    {
+        path: 'login', component: Login,
+    },
+    {
+        path: 'register', component: Register,
+    }
+];
--- client/src/app/core/layout/nav-bar/nav-bar.ts
+++ client/src/app/core/layout/nav-bar/nav-bar.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { removeSessionToken } from '../../auth/auth-storage.util.js';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.scss'
 })
