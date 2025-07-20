17f0329367562dae419df4ed0364bb97956b8473
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-07-20 13:14:27 +0300

    Client/Fix: fixed book details not rendering on fetched data

--- client/src/app/features/books/pages/details/details.ts
+++ client/src/app/features/books/pages/details/details.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { Book } from '../../models/index.js';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { CommentType } from '../../models/index.js';
 import { BooksService } from '../../books.service.js';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-details',
@@ -11,22 +12,37 @@ import { BooksService } from '../../books.service.js';
     templateUrl: './details.html',
     styleUrl: './details.scss'
 })
-export class Details implements OnInit {
+export class Details implements OnInit, OnDestroy {
     protected book: Book | null = null;
     protected comments: CommentType[] = [];
 
-    constructor(private bookService: BooksService) {
+    private subscriptions: Subscription = new Subscription();
+
+    constructor(private bookService: BooksService, private cdr: ChangeDetectorRef) {
+    }
+
+    ngOnDestroy(): void {
+        this.subscriptions?.unsubscribe();
     }
 
     ngOnInit(): void {
-        this.bookService.getBookWithOwner('c7d3e8f9-1a2b-3c4d-5e6f-7a8b9c0d1e2f').subscribe(data => {
-            this.book = data
+        const booksSub = this.bookService.getBookWithOwner('c7d3e8f9-1a2b-3c4d-5e6f-7a8b9c0d1e2f').subscribe(data => {
+            this.book = data;
+            this.cdr.detectChanges();
         });
-
-        this.bookService.getBookCommentsId('c7d3e8f9-1a2b-3c4d-5e6f-7a8b9c0d1e2f').subscribe(data => {
+        
+        const commentsIdsSub = this.bookService.getBookCommentsId('c7d3e8f9-1a2b-3c4d-5e6f-7a8b9c0d1e2f').subscribe(data => {
             data.forEach((value) => {
-                this.bookService.getCommentWithOwner(value._id).subscribe(data => this.comments?.push(data));
+                const commentsSub = this.bookService.getCommentWithOwner(value._id).subscribe(data => {
+                    this.comments?.push(data)
+                    this.cdr.detectChanges();
+                });
+
+                this.subscriptions?.add(commentsSub);
             });
         });
+
+        this.subscriptions?.add(booksSub);
+        this.subscriptions?.add(commentsIdsSub);
     }
 }
