bfda990ce266fb2a43b0981fe10bacfc9f5a19f0
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-07-30 23:43:30 +0300

    Client/Feat: set pathMatch to full in routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,8 +3,24 @@ import { Routes } from '@angular/router';
 import { Details } from './features/books/pages/details/details.js';
 
 export const routes: Routes = [
-    { path: 'login', loadComponent: () => import('./core/auth/pages/login/login.js').then(c => c.Login) },
-    { path: 'register', loadComponent: () => import('./core/auth/pages/register/register.js').then(c => c.Register) },  
-    { path: 'catalog', loadComponent: () => import('./features/books/pages/catalog/catalog.js').then(c => c.Catalog) },
-    { path: 'books/details/:bookId', loadComponent: () => import('./features/books/pages/details/details.js').then(c => c.Details) },
+    {
+        path: 'login',
+        loadComponent: () => import('./core/auth/pages/login/login.js').then(c => c.Login),
+        pathMatch: 'full',
+    },
+    {
+        path: 'register',
+        loadComponent: () => import('./core/auth/pages/register/register.js').then(c => c.Register),
+        pathMatch: 'full',
+    },  
+    {
+        path: 'catalog',
+        loadComponent: () => import('./features/books/pages/catalog/catalog.js').then(c => c.Catalog),
+        pathMatch: 'full',
+    },
+    {
+        path: 'books/details/:bookId',
+        loadComponent: () => import('./features/books/pages/details/details.js').then(c => c.Details),
+        pathMatch: 'full',
+    },
 ];
