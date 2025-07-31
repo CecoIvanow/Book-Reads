ea5e0090861684ab463cd1b5b4a070c454c4ea4f
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-07-31 21:29:37 +0300

    Client/Refactor: rename TokenAccessService to UserSessionService

---
+++ client/src/app/core/auth/services/user-session.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, signal } from '@angular/core';
+import { TokenSignal, UserSessionData } from '../models/index.js';
+import { USER_SESSION_KEY } from '../auth.const.js';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserSessionService {
+    userSession = signal<TokenSignal>(null);
+    private initPromise!: Promise<void>;
+
+    constructor() {
+        if (this.userSession() === null) {
+            this.initPromise = new Promise<void>(resolve => {
+                this.userSession.set(this.getAccessToken());
+                resolve();
+            })
+        }
+    }
+
+    async onInit() {
+        return this.initPromise;
+    }
+
+    private getAccessToken(): TokenSignal {
+        if (typeof sessionStorage !== 'undefined') {
+            const data = sessionStorage.getItem(USER_SESSION_KEY);
+            
+            return data ? JSON.parse(data) : null;
+        }
+
+        return 'none';
+    }
+
+    saveSessionToken(sessionData: UserSessionData) {
+        sessionStorage.setItem(USER_SESSION_KEY, JSON.stringify(sessionData));
+        this.userSession.set(sessionData);
+    }
+    
+    removeSessionToken() {
+        sessionStorage.removeItem(USER_SESSION_KEY);
+        this.userSession.set('none');
+    }
+}
