e2f5ea0ae75a63ca246050b8d5f70447fd21865b
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-08-01 18:05:21 +0300

    Client/Feat: implement onLogout auth server session delete call

--- client/src/app/core/auth/services/auth.service.ts
+++ client/src/app/core/auth/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { LoginCredentials, RegisterCredentials, AuthResponse } from '../models/index.js';
+import { LoginCredentials, RegisterCredentials, AuthResponse, AccessToken } from '../models/index.js';
 import { Observable } from 'rxjs';
 import { API_PATHS } from '../../../shared/constants/index.js';
 import { buildURL } from '../../../shared/utils/index.js';
@@ -22,5 +22,15 @@ export class AuthService {
         const url = buildURL(API_PATHS.USERS.REGISTER);
 
         return this.httpClient.post<AuthResponse>(url, body);
+    }
+
+    logout(token: AccessToken): Observable<undefined> {
+        const url = buildURL(API_PATHS.USERS.LOGOUT);
+
+        return this.httpClient.get<undefined>(url, {
+            headers: {
+                'X-Authorization': token
+            }
+        })
     }
 }
