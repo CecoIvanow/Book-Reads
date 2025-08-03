f881a7ba57f025280144ae0ce6fb662ba0b4c4ad
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-08-03 21:03:56 +0300

    Client/Feat: implement resolver data auto render in /books/edit

--- client/src/app/features/books/pages/edit/edit.ts
+++ client/src/app/features/books/pages/edit/edit.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { Book } from '../../models/book.model.js';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-edit',
@@ -11,6 +13,23 @@ import { RouterModule } from '@angular/router';
   templateUrl: './edit.html',
   styleUrl: './edit.scss'
 })
-export class Edit {
+export class Edit implements OnInit, OnDestroy {
+    protected book = signal<Book | null>(null);
 
+    private subscriptions = new Subscription();
+
+    constructor(private route: ActivatedRoute){
+    }
+
+    ngOnInit(): void {
+        const bookData: Book = this.route.snapshot.data['bookDetails'].at(0);
+
+        this.book.set(bookData);        
+    }
+
+    ngOnDestroy(): void {
+ 
+
+        this.subscriptions.unsubscribe();
+    }
 }
