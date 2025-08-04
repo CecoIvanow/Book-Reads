dd3ccc23840dad10fa93c30daeca5e9432e41d6a
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-08-05 00:46:23 +0300

    Client/Feat: migrate login form to reactive form approach

--- client/src/app/core/auth/pages/login/login.ts
+++ client/src/app/core/auth/pages/login/login.ts
@@ -2,35 +2,58 @@ import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
-import { AuthResponse, LoginCredentials } from '../../models/index.js';
+import { LoginCredentials } from '../../models/index.js';
 import { AuthService } from '../../services/auth.service.js';
 import { Router, RouterModule } from '@angular/router';
 import { UserSessionService } from '../../services/user-session.service.js';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-login',
-    imports: [MatInputModule, MatFormFieldModule, MatButtonModule, RouterModule],
+    imports: [
+        MatInputModule,
+        MatFormFieldModule,
+        MatButtonModule,
+        RouterModule,
+        ReactiveFormsModule
+    ],
     templateUrl: './login.html',
     styleUrl: './login.scss',
 })
 export class Login {
+    protected loginForm: FormGroup;
 
     constructor(
         private router: Router,
         private authService: AuthService,
-        protected userSession: UserSessionService) {
+        private formBuilder: FormBuilder,
+        protected userSession: UserSessionService,
+    ) {
+        this.loginForm = formBuilder.group({
+            'email': [
+                '',
+                [
+                    Validators.required,
+                    Validators.email,
+                ]
+            ],
+            'password': ['',
+                [
+                    Validators.required,
+                    Validators.minLength(4),
+                ]
+            ],
+        })
     }
 
-    onLogin(e: Event) {
-        e.preventDefault();
-
-        const formData = new FormData(e.currentTarget as HTMLFormElement);
-        const email = formData.get('email') as string;
-        const password = formData.get('password') as string;
+    onLogin() {
+        if (this.loginForm.invalid) {
+            return;
+        }
 
         const credentials: LoginCredentials = {
-            email,
-            password,
+            email: this.loginForm.value.email,
+            password: this.loginForm.value.password,
         }
 
         this.authService.login(credentials).subscribe({
