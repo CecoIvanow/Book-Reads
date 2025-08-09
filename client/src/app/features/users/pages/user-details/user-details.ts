3912a176b6b512241710617e80fb4e070ad7d73d
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-08-09 21:38:03 +0300

    Client/Feat: implement getBooksFromOwner method call in /users/details

--- client/src/app/features/users/pages/user-details/user-details.ts
+++ client/src/app/features/users/pages/user-details/user-details.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { Book, CommentType } from '../../../books/models/index.js';
 import { CommonModule } from '@angular/common';
-import { } from '@angular/common/http';
+import { BooksService, CommentsService } from '../../../books/services/index.js';
 import { UserSessionService } from '../../../../core/auth/services/user-session.service.js';
-import { Subscription } from 'rxjs';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
@@ -21,7 +20,7 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './user-details.html',
   styleUrl: './user-details.scss'
 })
-export class UserDetails {
+export class UserDetails implements OnInit {
     protected user = signal<string | null>(null);
     protected booksCount = signal<number>(0);
     protected commentsCount = signal<number>(0);
@@ -30,6 +29,20 @@ export class UserDetails {
 
     constructor(
         protected userSession: UserSessionService,
+        private booksService: BooksService,
+        private commentsService: CommentsService,
+        private route: ActivatedRoute,
     ) {
+    }
+
+    ngOnInit(): void {
+        const userId = this.route.snapshot.params['userId']
+        
+        this.booksService.getBooksFromOwner(userId).subscribe({
+            next: (books) => {
+                this.userBooks.set(books);
+                this.booksCount.set(books.length);
+            }
+        })
     }
 }
