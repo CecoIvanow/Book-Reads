c07d017ce63487551caf5feeac821bd0a8264703
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-08-13 18:56:41 +0300

    Client/Fix: fix console error on document due to SSR break/prerendering

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBar } from './core/layout/nav-bar/nav-bar.js';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
     selector: 'app-root',
@@ -11,7 +12,12 @@ import { NavBar } from './core/layout/nav-bar/nav-bar.js';
 export class App implements OnInit {
     protected title = 'Book-Reads';
 
+    constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    }
+
     ngOnInit() {
-        document.body.classList.add('angular-ready');
+        if (isPlatformBrowser(this.platformId)) {
+            document.body.classList.add('angular-ready');
+        }
     }
 }
